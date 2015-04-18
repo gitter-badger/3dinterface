106fe443add32ea647ed2bbc075905d425297226
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-04-18 17:30:04 +0200

    Revert "Changed index.py to test django"
    This reverts commit 443f12f5533ad447a3c36df06bf0ac53dd630422.

--- index.py
+++ index.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from django import template
-from django.conf import settings
+import sys
+from webtools import Web
 
-settings.configure()
-t = template.Template(open('index.html','r').read())
-c = template.Context({})
-v = t.render(c)
-
-print('Content-type: text/html')
-print()
-
-print(v)
-
-
-# Web.render('index.html')
+Web.render('index.html')
