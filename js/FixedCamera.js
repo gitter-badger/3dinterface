4a125b95c183236743aef72548fd09bb830d1d5e
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-04-23 10:48:40 +0200

    Transparency

--- js/FixedCamera.js
+++ js/FixedCamera.js
@@ -134,7 +134,8 @@ FixedCamera.prototype.update = function(mainCamera) {
             elt.material.transparent =   new_value < 0.9;
             elt.material.opacity = new_value;
 
-            elt.material.transparent = elt.visible = new_value > 0.05;
+            if (new_value < 0.05)
+                elt.material.transparent = elt.visible = false;
         }
     });
 
