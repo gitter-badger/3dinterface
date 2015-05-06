d677390bb25368674f08c36e8d37da38810070e6
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-05-06 17:05:56 +0200

    Removed useless console.log

--- controllers/index/index.js
+++ controllers/index/index.js
@@ -2,7 +2,6 @@ module.exports.index = function(req, res) {
     res.setHeader('Content-Type', 'text/html');
 
     res.render('index.jade', res.locals, function(err, out) {
-        console.log(err);
         res.send(out);
     });
 }
--- controllers/stream/index.js
+++ controllers/stream/index.js
@@ -20,7 +20,6 @@ module.exports.index = function(req, res, next) {
 
     res.setHeader('Content-Type', 'text/html');
     res.render('index.jade', res.locals, function(err, result) {
-        console.log(err);
         res.send(result);
     });
 }
