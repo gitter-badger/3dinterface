31895cf12c08c1c2cbc4318ed6d0027d9c987d49
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-05-18 15:36:24 +0200

    Added forgotten file

---
+++ static/js/Logger.js
@@ -0,0 +1,20 @@
+var BD = {};
+
+BD.Event = {};
+
+BD.Event.ArrowClicked = function() {};
+BD.Event.ArrowClicked.prototype.send = function() {
+    var url = "http://localhost:4000/post";
+
+    var data = {};
+
+    data.user_id = this.user_id;
+    data.arrow_id = this.arrow_id;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(data));
+
+};
+
