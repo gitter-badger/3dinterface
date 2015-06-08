7c62c0ab3454e6fd14a73b363477ab16e2e46cc7
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-06-08 14:59:00 +0200

    Re-enable logging

--- js/prototype/main.js
+++ js/prototype/main.js
@@ -79,7 +79,6 @@ function init() {
     // Initialize pointer camera
     var camera1 = new PointerCamera(50, container_size.width() / container_size.height(), 0.1, 100000, renderer, container);
 
-    BD.disable();
     cameras = initMainScene(camera1, scene, static_path, coins);
     // cameras = initPeach(camera1, scene, static_path, coins);
     // cameras = initBobomb(camera1, scene, static_path, coins);
