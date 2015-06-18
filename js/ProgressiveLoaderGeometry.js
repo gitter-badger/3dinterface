0b4c01d3e0f1e2233b243b0621dddeaeef98e3de
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-06-18 16:36:16 +0200

    Clean

--- js/ProgressiveLoaderGeometry.js
+++ js/ProgressiveLoaderGeometry.js
@@ -111,12 +111,8 @@ ProgressiveLoaderGeometry.prototype.load = function() {
 }
 
 ProgressiveLoaderGeometry.prototype.getCamera = function() {
-    if (this.camera !== null)
-        return [this.camera.position.x, this.camera.position.y, this.camera.position.z,
-                this.camera.target.x,   this.camera.target.y,   this.camera.target.z];
-    else
-       return null;
-
+    return [this.camera.position.x, this.camera.position.y, this.camera.position.z,
+        this.camera.target.x,   this.camera.target.y,   this.camera.target.z];
 }
 
 ProgressiveLoaderGeometry.prototype.initIOCallbacks = function() {
