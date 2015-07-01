f18afb44716e2025d58c4fa7611080f73bef3014
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-07-01 11:00:05 +0200

    Redirect after logout (much classier)

--- controllers/logout/index.js
+++ controllers/logout/index.js
@@ -1,9 +1,7 @@
 module.exports.index = function(req, res) {
+
     req.session = null;
     res.locals.session = null;
-    res.setHeader('Content-Type', 'text/html');
+    res.redirect('/');
 
-    res.render('../../index/views/index.jade', res.locals, function(err, out) {
-        res.send(out);
-    });
 };
