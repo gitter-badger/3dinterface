29b7760bec21325bb937619679d7ff83bb6ead90
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-07-02 14:37:07 +0200

    Added log in socket

--- socket.js
+++ socket.js
@@ -4,6 +4,8 @@ var geo = require('./lib/geo.min.js');
 module.exports = function(io) {
     io.on('connection', function(socket) {
 
+        console.log('[SOK] ' + new Date() + ' ' + socket.conn.remoteAddress);
+
         var streamer = new geo.MeshStreamer();
 
         streamer.start(socket);
