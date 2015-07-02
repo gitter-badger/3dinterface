b5c80d46039476feb94c5e2cab5cb7a0e592021b
Thomas FORGIONE <thomas.forgione@gmail.com>
2015-07-02 14:42:54 +0200

    Log more stuff

--- socket.js
+++ socket.js
@@ -4,7 +4,11 @@ var geo = require('./lib/geo.min.js');
 module.exports = function(io) {
     io.on('connection', function(socket) {
 
-        console.log('[SOK] ' + new Date() + ' ' + socket.conn.remoteAddress);
+        console.log('[SOK] ' + new Date() + ' ' + socket.conn.remoteAddress + ' connection');
+
+        socket.on('disconnect', function() {
+            console.log('[SOK] ' + new Date() + ' ' + socket.conn.remoteAddress + ' disconnect');
+        });
 
         var streamer = new geo.MeshStreamer();
 
