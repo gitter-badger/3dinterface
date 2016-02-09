2d0aa0a1a0b78cd7463b72d4aa713f5d0d9da524
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-02-09 18:01:16 +0100

    Migrating to streams

--- tasks/prepare-L3D.js
+++ tasks/prepare-L3D.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var changed = require('gulp-changed');
 var path = require('path');
-var merge = require('merge-dirs').default;
 var mkdirp = require('mkdirp');
 var exec = require('child_process').exec;
 
@@ -8,24 +9,39 @@ var root = path.join(__dirname, '..');
 var rootL3D = path.join(root, 'js/L3D');
 
 gulp.task('prepare-L3D-npm', function(done) {
-    exec('cd ' + rootL3D + ' && npm install', done);
+
+    var src = path.join(rootL3D, 'package.json');
+    var dest = path.join(rootL3D, 'node_modules');
+
+    return gulp.src(src)
+        .pipe(changed(dest))
+        .pipe(shell('npm install', {cwd:rootL3D}))
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task('prepare-L3D-tsd-typings', function(done) {
-    exec('cd ' + rootL3D + ' && tsd install', done);
+
+    var src = path.join(rootL3D, 'tsd.json');
+    var dest = path.join(rootL3D, 'typings');
+
+    return gulp.src(src)
+        .pipe(changed(dest))
+        .pipe(shell('tsd install', {cwd:rootL3D}))
+        .pipe(gulp.dest(dest));
+
 });
 
 gulp.task('prepare-L3D-custom-typings', function(done) {
-    process.chdir(root);
-    mkdirp('./js/L3D/typings');
-    merge('./custom_typings', './js/L3D/typings');
-    done();
+
+    var src = path.join(root, 'custom_typings') + '/*';
+    var dest = path.join(rootL3D, 'typings');
+
+    return gulp.src(src)
+        .pipe(changed(dest))
+        .pipe(gulp.dest(dest));
+
 });
 
-gulp.task('prepare-L3D-typings', ['prepare-L3D-tsd-typings', 'prepare-L3D-custom-typings'], function(done) {
-    done();
-});
+gulp.task('prepare-L3D-typings', ['prepare-L3D-tsd-typings', 'prepare-L3D-custom-typings']);
 
-gulp.task('prepare-L3D', ['prepare-L3D-npm', 'prepare-L3D-typings'], function(done) {
-    done();
-});
+gulp.task('prepare-L3D', ['prepare-L3D-npm', 'prepare-L3D-typings']);
