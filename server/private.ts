d59248ec640a53ac1664b01d4cd0d9e4096f5a83
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-02-10 16:56:03 +0100

    Added dummy private.ts

---
+++ server/private.ts
@@ -0,0 +1,11 @@
+/** @hidden */
+export var url  : string = null
+
+/** @hidden */
+export var secret : string = null;
+
+/** @hidden */
+export var microSecretKey : string = null;
+
+/** @hidden */
+export var microCampaignId : string = null;
