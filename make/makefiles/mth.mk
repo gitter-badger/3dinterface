27faa8236b7f4dcfff23c5a9f081473a4b924300
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-02-19 16:16:24 +0100

    Corrected path of .dirstamp

--- make/makefiles/mth.mk
+++ make/makefiles/mth.mk
@@ -5,11 +5,11 @@ src/mth/typings/.dirstamp: src/mth/typings.json
 	$(CD) src/mth/ && $(TYPINGS) install
 	$(TOUCH_DIRSTAMP)
 
-src/mth/typings/node_modules/.dirstamp: src/mth/package.json
+src/mth/node_modules/.dirstamp: src/mth/package.json
 	$(CD) src/mth/ && $(NPM) install
 	$(TOUCH_DIRSTAMP)
 
-src/mth/build/.dirstamp: src/mth/*.ts src/mth/package.json src/mth/tsconfig.json src/mth/typings/.dirstamp src/mth/typings/node_modules/.dirstamp
+src/mth/build/.dirstamp: src/mth/*.ts src/mth/package.json src/mth/tsconfig.json src/mth/typings/.dirstamp src/mth/node_modules/.dirstamp
 	$(CD) src/mth/ && $(TSC)
 	$(TOUCH_DIRSTAMP)
 
