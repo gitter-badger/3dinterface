cd29b755441e3b5910df46fe6c4d388af04c8499
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-02-19 23:05:47 +0100

    Cleaned test, and improve mth_test in mth.mk

--- make/makefiles/mth.mk
+++ make/makefiles/mth.mk
@@ -9,9 +9,9 @@ src/mth/node_modules/.dirstamp: src/mth/package.json
 	$(CD) src/mth/ && $(NPM) install
 	$(TOUCH_DIRSTAMP)
 
-src/mth/build/.dirstamp: src/mth/*.ts src/mth/package.json src/mth/tsconfig.json src/mth/typings/.dirstamp src/mth/node_modules/.dirstamp
+src/mth/build/.dirstamp: src/mth/*.ts src/mth/package.json src/mth/tsconfig.json src/mth/typings/.dirstamp src/mth/node_modules/.dirstamp src/mth/tests/*.ts
 	$(CD) src/mth/ && $(TSC)
 	$(TOUCH_DIRSTAMP)
 
-mth_test: $(MTH_COMMONJS_DEPENDENCY) src/mth/tests/*.ts
+mth_test: $(MTH_COMMONJS_DEPENDENCY)
 	$(NODEUNIT) src/mth/build/tests/main.js
--- src/mth/tests/main.ts
+++ src/mth/tests/main.ts
@@ -9,12 +9,21 @@ let v2 = new THREE.Vector3(2,3,4);
 let v1Bak = v1.clone();
 let v2Bak = v2.clone();
 
+function cmp_vector(test : t.Test, v1 : mth.Vector3, v2 : mth.Vector3) {
+
+    test.equal(v1.x, v2.x);
+    test.equal(v1.y, v2.y);
+    test.equal(v1.z, v2.z);
+
+}
+
 export function equals_test(test : t.Test) {
 
     test.ok(mth.equals(v1,v1));
     test.ok(mth.equals(v2,v2));
     test.ok(!mth.equals(v1,v2));
     test.ok(!mth.equals(v2,v1));
+
     test.done();
 
 }
@@ -22,9 +31,14 @@ export function equals_test(test : t.Test) {
 export function sum_test(test : t.Test) {
 
     let v3 = mth.sum(v1,v2);
-    test.ok(v3.x == v1.x + v2.x && v3.y == v1.y + v2.y && v3.z == v1.z + v2.z);
-    test.ok(mth.equals(v1, v1Bak));
-    test.ok(mth.equals(v2, v2Bak));
+
+    test.equal(v3.x, v1.x + v2.x);
+    test.equal(v3.y, v1.y + v2.y);
+    test.equal(v3.z, v1.z + v2.z);
+
+    cmp_vector(test, v1, v1Bak);
+    cmp_vector(test, v2, v2Bak);
+
     test.done();
 
 }
@@ -32,9 +46,12 @@ export function sum_test(test : t.Test) {
 export function diff_test(test : t.Test) {
 
     let v5 = mth.dot(v1,v2);
-    test.ok(v5 == v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
-    test.ok(mth.equals(v1, v1Bak));
-    test.ok(mth.equals(v2, v2Bak));
+
+    test.equal(v5, v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
+
+    cmp_vector(test, v1, v1Bak);
+    cmp_vector(test, v2, v2Bak);
+
     test.done();
 
 }
@@ -42,9 +59,12 @@ export function diff_test(test : t.Test) {
 export function cross_test(test : t.Test) {
 
     let v5 = mth.dot(v1,v2);
-    test.ok(v5 == v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
-    test.ok(mth.equals(v1, v1Bak));
-    test.ok(mth.equals(v2, v2Bak));
+
+    test.equal(v5, v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
+
+    cmp_vector(test, v1, v1Bak);
+    cmp_vector(test, v2, v2Bak);
+
     test.done();
 
 }
@@ -53,20 +73,37 @@ for (let lambda = 0; lambda < 5; lambda += 0.5) {
 
     exports['mul_test_' + (2*lambda+1)] = function(test : t.Test) {
 
-        let v7 = mth.mul(v1, lambda);
-        test.ok(mth.equals(v7, new THREE.Vector3(v1Bak.x*lambda, v1Bak.y*lambda, v1Bak.z*lambda)));
         v1 = v1Bak.clone();
         v2 = v2Bak.clone();
+
+        let v7 = mth.mul(v1, lambda);
+
+        test.equal(v7.x, v1Bak.x*lambda);
+        test.equal(v7.y, v1Bak.y*lambda);
+        test.equal(v7.z, v1Bak.z*lambda);
+
+        cmp_vector(test, v1, v1Bak);
+
+        v1 = v1Bak.clone();
+        v2 = v2Bak.clone();
+
         let v8 = mth.mul(v1, lambda);
-        test.ok(mth.equals(v8, new THREE.Vector3(v1Bak.x*lambda, v1Bak.y*lambda, v1Bak.z*lambda)));
+
+        test.equal(v8.x, v1Bak.x * lambda);
+        test.equal(v8.y, v1Bak.y * lambda);
+        test.equal(v8.z, v1Bak.z * lambda);
+
+        cmp_vector(test, v1, v1Bak);
+
         v1 = v1Bak.clone();
         v2 = v2Bak.clone();
 
         // Try into v1
         v1 = mth.mul(v1, lambda);
-        test.ok(mth.equals(v1, new THREE.Vector3(v1Bak.x*lambda, v1Bak.y*lambda, v1Bak.z*lambda)));
-        v1 = v1Bak.clone();
-        v2 = v2Bak.clone();
+
+        test.equal(v7.x, v1Bak.x*lambda);
+        test.equal(v7.y, v1Bak.y*lambda);
+        test.equal(v7.z, v1Bak.z*lambda);
 
         test.done();
 
