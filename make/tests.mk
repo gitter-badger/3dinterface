59b4f07487e70118b49213fa3a02bed6ff60408d
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-02-21 11:54:31 +0100

    This should not have 100% success...

--- make/tests.mk
+++ make/tests.mk
@@ -2,7 +2,7 @@ TEST_FILES_SRC=$(wildcard src/*/tests/main.ts)
 TEST_OBJ=$(subst tests/main.ts,build/tests/main.js,$(TEST_FILES_SRC))
 TEST_COV=$(TEST_OBJ:.js=-cov.js)
 
-src/%/build/tests/main-cov.js: src/%/build/tests/main.js
+src/%/build/tests/main-cov.js: src/%/build/tests/main.js $(MTH_COMMONJS_DEPENDENCY)
 	jscoverage $<
 
 test-%: src/%/build/tests/main-cov.js
