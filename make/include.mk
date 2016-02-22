33d0cb713345973eeb781cee2be539aa659a2814
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-02-22 11:11:15 +0100

    Added begining of demo

--- make/include.mk
+++ make/include.mk
@@ -6,3 +6,4 @@ include ./make/makefiles/l3d.mk
 include ./make/makefiles/l3dp.mk
 include ./make/makefiles/bouncing.mk
 include ./make/makefiles/server.mk
+include ./make/makefiles/demo.mk
---
+++ src/demo/config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, './main.ts'),
+    output: {
+        libraryTarget: 'var',
+        library: 'config',
+        filename: path.join(__dirname, '../server/build/static/js/demo.js'),
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.json']
+    },
+    module: {
+        loaders: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader'
+        },
+        {
+            test: /\.json$/,
+            loader: 'json-loader'
+        }],
+        exclude: /node_modules/
+    },
+    externals: {
+
+    },
+    plugins: [
+        require('webpack-fail-plugin')
+    ],
+    devtool:'sourcemap',
+    ts: {
+        configFileName:'./tsconfig.json'
+    }
+};
